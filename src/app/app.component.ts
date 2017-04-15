d5f8158299860e33ddd59aa25445c2a008221a5c
Yadagalla Jaswanth Kumar <yadagallajaswanthkumar98@gmail.com>
2017-04-15 13:49:54 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input, OnChanges, SimpleChange } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+	myEvent(event) {
+		console.log(event)
+	}
+}
